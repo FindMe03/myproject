68d5e89cc7ec22b4010b9dedc111b0ca60b981f4
FindMe03 <mahonwa01zerone@gmail.com>
2021-08-06 05:49:29 +0800

    pictures

--- index.php
+++ index.php
@@ -67,7 +67,7 @@
           <a href="#" class="stretched-link">Continue reading</a>
         </div>
         <div class="col-auto d-none d-lg-block">
-         <img src="assets/images/QueenMutter.jpg" width="100%" height="100%">
+        <img src="assets/images/QueenMutter.jpg" width="100%" height="100%">
 
         </div>
       </div>
@@ -82,7 +82,7 @@
           <a href="#" class="stretched-link">Continue reading</a>
         </div>
         <div class="col-auto d-none d-lg-block">
-          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+        <img src="assets/images/morris.jpg" width="100%" height="100%">
 
         </div>
       </div>
